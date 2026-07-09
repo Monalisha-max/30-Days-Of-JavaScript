902ed6c387b17f98765c416c13e0bc63c7e38836
amarselenium <amareshkumarrout36@gmail.com>
2026-07-09 23:02:23 +0530

    resolving conflicts

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-console.log("my name is Monalisa");
-=======
+
 console.log("this is my object");
 // addrssed
 console.log("this is my addressed file");
->>>>>>> a4c0be4c5a2bf3c800ebe75c4ffa4d2fcef74555
+console.log("my name is Monalisa");
